e040e528d7437837c324c1112a76dde8888d35da
Ashuajax <yesaj9354@gmail.com>
2024-11-07 19:48:37 +0530

    test push

--- src/Comp/DynamicForm.tsx
+++ src/Comp/DynamicForm.tsx
@@ -1,6 +1,32 @@
 import React from 'react'
 
 function DynamicForm() {
+    const formProperties = {
+        configs: {
+          type: "object",
+          properties: {
+            clientId: {
+              type: "string",
+              title: "Client Id",
+              description: "Client Id shared by JioPay toStatusResponse merchant",
+              minLength: 1,
+            },
+            clientSecret: {
+              type: "string",
+              title: "Client Secret",
+              description:
+                "Client Secret shared by JioPay toStatusResponse merchant",
+              minLength: 5,
+            },
+            pspMid: {
+              type: "string",
+              title: "Merchant Id",
+              description: "Merchant Id shared by JioPay toStatusResponse merchant",
+              minLength: 1,
+            },
+          },
+        },
+      };
   return (
     <div>DynamicForm</div>
   )
