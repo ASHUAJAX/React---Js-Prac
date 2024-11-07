5995d740c47600bd08f4f6f32fd1e2e522087e2c
Yardy-Ashu <Ashutosh.jha@searchingyard.com>
2024-11-07 19:46:41 +0530

    commit and push

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Prac from './assets/Comp/Prac'
-import Crud from './assets/Comp/Crud'
+import Prac from './Comp/Prac'
+import Crud from './Comp/Crud'
+import DynamicForm from './Comp/DynamicForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <Crud/>
+  {/* <Crud/> */}
+  <DynamicForm/>
     </>
   )
 }
